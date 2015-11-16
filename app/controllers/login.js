86e9297559c170647385b1e2440f19ab39a2c375
Matteo <mazzarolomatteo@gmail.com>
2015-11-16 14:33:08 +0100

    Refactored HTTP call

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -15,56 +15,60 @@ function login(e) {
     'password': password
   }
 
-  var loader = Titanium.Network.createHTTPClient()
-
-  // Function called when the response data is available
-  loader.onload = function(res) {
-    Titanium.API.info('onload')
-    Titanium.API.info('Status: ' + this.status)
-    Titanium.API.info('ResponseText: ' + this.responseText)
-    Titanium.API.info('ConnectionType: ' + this.connectionType)
-    Titanium.API.info('Location: ' + this.location)
-
-    endLoading()
-    var token = this.responseText.token
-    Ti.App.Properties.setString('token', token)
-    Ti.App.Properties.setString('email', params.email)
-    Alloy.createController('userBookmarks').getView().open();
-  }
-
-  // Function called when an error occurs, including a timeout
-  loader.onerror = function(e) {
-    Titanium.API.info('onerror')
-    Titanium.API.info('JSON.stringify: ' + JSON.stringify(e))
-    Titanium.API.info('Error: ' + e.error)
-    Titanium.API.info('ResponseText: ' + this.responseText)
-    Titanium.API.info('ConnectionType: ' + this.connectionType)
-    Titanium.API.info('Location: ' + this.location)
-
-    endLoading()
-    Ti.App.Properties.removeProperty('token')
-    Ti.App.Properties.removeProperty('email')
-    var responseJson = JSON.parse(this.responseText)
-    if (this.status == 422) {
-      var errors = responseJson.errors
-      for (var i = 0; i < errors.length; i++) {
-        err = errors[i]
-        $.errorLabel.text = $.errorLabel.text + '\n' + err.message
-      }
-    } else {
-      $.errorLabel.text = responseJson.message
-    }
-  }
-
   // Prepare and start the API call
+  var loader = Titanium.Network.createHTTPClient()
+  loader.onload = onLoginComplete
+  loader.onerror = onLoginError
   loader.open('POST', url)
   loader.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
   loader.send(params)
-  $.errorLabel.text = ''
+
+  // Show the loading spinner
   startLoading()
 }
 
+
+// Function called when the response data is available
+function onLoginComplete(res) {
+  Titanium.API.info('onLoginComplete')
+  Titanium.API.info('Status: ' + this.status)
+  Titanium.API.info('ResponseText: ' + this.responseText)
+  Titanium.API.info('ConnectionType: ' + this.connectionType)
+  Titanium.API.info('Location: ' + this.location)
+
+  endLoading()
+  var token = this.responseText.token
+  Ti.App.Properties.setString('token', token)
+  Ti.App.Properties.setString('email', $.emailTextField.value)
+  Alloy.createController('userBookmarks').getView().open();
+}
+
+// Function called when an error occurs, including a timeout
+function onLoginError(e) {
+  Titanium.API.info('onLoginError')
+  Titanium.API.info('JSON.stringify: ' + JSON.stringify(e))
+  Titanium.API.info('Error: ' + e.error)
+  Titanium.API.info('ResponseText: ' + this.responseText)
+  Titanium.API.info('ConnectionType: ' + this.connectionType)
+  Titanium.API.info('Location: ' + this.location)
+
+  endLoading()
+  Ti.App.Properties.removeProperty('token')
+  Ti.App.Properties.removeProperty('email')
+  var responseJson = JSON.parse(this.responseText)
+  if (this.status == 422) {
+    var errors = responseJson.errors
+    for (var i = 0; i < errors.length; i++) {
+      err = errors[i]
+      $.errorLabel.text = $.errorLabel.text + '\n' + err.message
+    }
+  } else {
+    $.errorLabel.text = responseJson.message
+  }
+}
+
 function startLoading() {
+  $.errorLabel.text = ''
   $.activityIndicator.show()
   $.emailTextField.setEnabled(false)
   $.passwordTextField.setEnabled(false)
